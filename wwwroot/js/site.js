72d5c966e0c489dbb74c9f6b507eba28dfe38387
Jonatas Bazzoli <jonatasbazzoli@gmail.com>
2020-01-07 23:35:44 +0100

    dataTable

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+
+    // Set up the DataTable grid
+    $('#serverTable').DataTable();
+    $('#sinomimo').DataTable();
+});
